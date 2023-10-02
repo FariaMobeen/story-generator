91b1e20281135e0bc2b3ff2d1f46d0251940c403
FariaMobeen <faria.mobeen@ucalgary.ca>
2023-10-01 19:48:34 -0600

    date

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,8 +1,32 @@
-function FormattedDate({ date }) {
-  const options = { year: 'numeric', month: 'long', day: 'numeric' };
-  const formattedDate = new Date(date).toLocaleDateString(undefined, options);
-  return <span>{formattedDate}</span>;
+function getOrdinalSuffix(day) {
+  if (day >= 11 && day <= 13) {
+    return 'th';
+  }
+  switch (day % 10) {
+    case 1:
+      return 'st';
+    case 2:
+      return 'nd';
+    case 3:
+      return 'rd';
+    default:
+      return 'th';
+  }
 }
 
-  
-  export default FormattedDate;
+function FormattedDate({ date }) {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  const dateObject = new Date(date);
+  const day = dateObject.getDate();
+  const month = dateObject.toLocaleString('default', { month: 'long' });
+  const year = dateObject.getFullYear();
+  const ordinalSuffix = getOrdinalSuffix(day);
+
+  return (
+    <span>
+      {`${day}${ordinalSuffix} ${month} ${year}`}
+    </span>
+  );
+}
+
+export default FormattedDate;
